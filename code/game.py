8166972cacfbd631a6fe91708504c9de4891d110
StephaniaPucci <stephaniamariapucci@gmail.com>
2026-06-08 14:20:13 -0300

    Creating main loop Connecting Menu and Game Classes

--- code/game.py
+++ code/game.py
@@ -8,9 +8,8 @@ from code.menu import Menu
 
 class Game:
     def __init__(self):
-        self.window = None
         pygame.init()
-        window = pygame.display.set_mode(size=(600, 480))
+        self.window = pygame.display.set_mode(size=(600, 480))
 
     def run(self, ):
         while True:
